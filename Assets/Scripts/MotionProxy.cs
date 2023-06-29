58a70862ba6d052aae3dfc570e7bf618b8b9b4db
Baek In-Chang <inchang.baek@gm.gist.ac.kr>
2023-06-29 22:11:58 +0100

    Done

--- Assets/Scripts/MotionProxy.cs
+++ Assets/Scripts/MotionProxy.cs
@@ -39,9 +39,6 @@ namespace Mediapipe.Unity.PoseTracking
         void Start()
         {
             // 
-
-
-
         }
 
         // Update is called once per frame
@@ -92,7 +89,7 @@ namespace Mediapipe.Unity.PoseTracking
                     {
 
 
-                        if (Mathf.Abs(angle[2]) < 0.04)
+                        if (Mathf.Abs(angle[2]) < 0.015)
                         {
                             tmp_decision_forward *= 0;
                         }
@@ -102,7 +99,7 @@ namespace Mediapipe.Unity.PoseTracking
                             tmp_decision_forward *= angle[2];
                         }
 
-                        if (Mathf.Abs(angle[0]) < 0.01)
+                        if (Mathf.Abs(angle[0]) < 0.005)
                         {
 
                             tmp_decision_backward *= 0;
@@ -120,7 +117,7 @@ namespace Mediapipe.Unity.PoseTracking
             }
             frame += 1;
             
-            if (Mathf.Abs(tmp_decision_forward) > 0 || Mathf.Abs(tmp_decision_forward) > Mathf.Abs(tmp_decision_backward))
+            if (tmp_decision_forward > 0 || Mathf.Abs(tmp_decision_forward) > Mathf.Abs(tmp_decision_backward))
             {
 
                 return +2.0f;
